9adc0e27c79ba89f39637d4c3800b31ee0c5ffd0
jongjin0920 <playck@eoding.com>
2025-09-25 18:00:07 +0900

    fix: next 이미지 관련 config 수정

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "*.supabase.co", "supabase.co"],
+    domains: ["images.unsplash.com"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "*.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "owekjzhkudjhwgqjttte.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
