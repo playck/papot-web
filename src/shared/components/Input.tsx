9ea4f0fcfd27559660e32f814d9c58eb52c3b2b4
jongjin0920 <playck@eoding.com>
2025-11-28 18:52:31 +0900

    fix: Input 컴포넌트 useId훅으로 고유 ID 생성되도록 수정 (하이드레이션 에러 수정)

--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { forwardRef, InputHTMLAttributes } from "react";
+import { forwardRef, InputHTMLAttributes, useId } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -24,7 +24,8 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     },
     ref
   ) => {
-    const inputId = id || `input-${Math.random().toString(36).substr(2, 9)}`;
+    const generatedId = useId();
+    const inputId = id || generatedId;
 
     const baseClasses =
       "px-3 py-2 border rounded-md transition-colors focus:outline-none focus:ring-2";
